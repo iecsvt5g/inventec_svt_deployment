2e146e613f78f1a9b51c39ee8ef6f198213ed711
ZL Chen <iec100535@gmail.com>
2022-09-29 13:02:46 +0800

    phy_bler_parser.py

--- phy_bler_parser.py
+++ phy_bler_parser.py
@@ -5,7 +5,8 @@ Created on 2022/09/01
 @author: ZL Chen
 @title: PHY BLER Parser
 '''
-import sys
+
+# import sys
 # sys.path.insert(0, '../lib')
 import subprocess
 import pymysql
@@ -15,14 +16,22 @@ from time import sleep
 
 class bler_parser(object):
 	def parser(self):
-		parser_cli_0 = 'grep \"0 (Kbps)\" /home/BaiBBU_XSS/BaiBBU_PXSS/PHY/bin/Phy.log | awk \'END{print $(NF-4)}\' | sed \'s/.$//\''
-		parser_cli_1 = 'grep \"1 (Kbps)\" /home/BaiBBU_XSS/BaiBBU_PXSS/PHY/bin/Phy.log | awk \'END{print $(NF-4)}\' | sed \'s/.$//\''
-		parser_response_0 = str(subprocess.check_output(parser_cli_0, shell=True)).split('b\'')[1]
-		parser_response_1 = str(subprocess.check_output(parser_cli_1, shell=True)).split('b\'')[1]
-		parser_response_0 = float(parser_response_0[0:-3])
-		parser_response_1 = float(parser_response_1[0:-3])
-		return parser_response_0, parser_response_1
+		while True:
+			parser_cli_0 = 'grep \"0 (Kbps)\" /home/BaiBBU_XSS/BaiBBU_PXSS/PHY/bin/Phy.log | awk \'END{print $(NF-4)}\' | sed \'s/.$//\''
+			parser_cli_1 = 'grep \"1 (Kbps)\" /home/BaiBBU_XSS/BaiBBU_PXSS/PHY/bin/Phy.log | awk \'END{print $(NF-4)}\' | sed \'s/.$//\''
+			parser_response_0 = str(subprocess.check_output(parser_cli_0, shell=True)).split('b\'')[1]
+			parser_response_1 = str(subprocess.check_output(parser_cli_1, shell=True)).split('b\'')[1]
+			if parser_response_0[0].isdigit():
+				parser_response_0 = float(parser_response_0[0:-3])
+				parser_response_1 = float(parser_response_1[0:-3])
+				# print(parser_response_0)
+				print(parser_response_0, parser_response_1)
+				break
+			else:
+				time.sleep(1)
+				pass
 		# return parser_response_0
+		return parser_response_0, parser_response_1
 	
 	def cloud_db(self, _time, r_0, r_1):
 		try:
@@ -41,7 +50,7 @@ class bler_parser(object):
 			conn = pymysql.connect(**sql)
 			cur = conn.cursor()
 			sql = """INSERT INTO {table}(time, ip, phy_bler_0, phy_bler_1) VALUES(%s, %s, %s, %s)""".format(table='phy_bler_parser')
-			# cur.execute(sql, (_time, '172.32.3.155', r_0))
+			# cur.execute(sql, (_time, '172.32.3.164', r_0))
 			cur.execute(sql, (_time, '172.32.3.199', r_0, r_1))
 			conn.commit()
 		except Exception as e:
