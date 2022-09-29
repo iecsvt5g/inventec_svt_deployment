c30f281890ca70c452c2e328a09a217b97f0373a
chen.zl <iecsvt5g@gmail.com>
2022-09-29 23:22:21 +0800

    bbu_status_check, phy_bler_parser update

--- phy_bler_parser.py
+++ phy_bler_parser.py
@@ -13,27 +13,42 @@ import pymysql
 import time
 # from influx_db import infulx_db
 from time import sleep
+from subprocess import check_output
 
 class bler_parser(object):
 	def parser(self):
 		while True:
 			parser_cli_0 = 'grep \"0 (Kbps)\" /home/BaiBBU_XSS/BaiBBU_PXSS/PHY/bin/Phy.log | awk \'END{print $(NF-4)}\' | sed \'s/.$//\''
-			parser_cli_1 = 'grep \"1 (Kbps)\" /home/BaiBBU_XSS/BaiBBU_PXSS/PHY/bin/Phy.log | awk \'END{print $(NF-4)}\' | sed \'s/.$//\''
+			# parser_cli_1 = 'grep \"1 (Kbps)\" /home/BaiBBU_XSS/BaiBBU_PXSS/PHY/bin/Phy.log | awk \'END{print $(NF-4)}\' | sed \'s/.$//\''
 			parser_response_0 = str(subprocess.check_output(parser_cli_0, shell=True)).split('b\'')[1]
-			parser_response_1 = str(subprocess.check_output(parser_cli_1, shell=True)).split('b\'')[1]
+			# parser_response_1 = str(subprocess.check_output(parser_cli_1, shell=True)).split('b\'')[1]
 			if parser_response_0[0].isdigit():
 				parser_response_0 = float(parser_response_0[0:-3])
-				parser_response_1 = float(parser_response_1[0:-3])
-				# print(parser_response_0)
-				print(parser_response_0, parser_response_1)
+				# parser_response_1 = float(parser_response_1[0:-3])
+				print(parser_response_0)
+				# print(parser_response_0, parser_response_1)
 				break
 			else:
 				time.sleep(1)
 				pass
-		# return parser_response_0
-		return parser_response_0, parser_response_1
-	
-	def cloud_db(self, _time, r_0, r_1):
+		return parser_response_0
+		# return parser_response_0, parser_response_1
+
+	'''
+	IP Address Parser
+	'''
+	def _ip_parser(self):
+		try:
+			ip_command = 'ip addr | grep \'inet 172.32\' | awk \'END{print $2}\''
+			ip_command = check_output(ip_command, shell=True).decode("utf-8").strip()
+			ip_command = ip_command.replace('/24', '')	# replace subnet mask str
+			ip_command = ip_command.replace('/16', '')	# replace subnet mask str
+			return ip_command
+		except:
+			return 'Not Found IP'
+
+	def cloud_db(self, _time, r_0):
+	# def cloud_db(self, _time, r_0, r_1):
 		try:
 			sql = {
 					# 'host': '20.212.112.202',
@@ -49,9 +64,12 @@ class bler_parser(object):
 					}
 			conn = pymysql.connect(**sql)
 			cur = conn.cursor()
-			sql = """INSERT INTO {table}(time, ip, phy_bler_0, phy_bler_1) VALUES(%s, %s, %s, %s)""".format(table='phy_bler_parser')
-			# cur.execute(sql, (_time, '172.32.3.164', r_0))
-			cur.execute(sql, (_time, '172.32.3.199', r_0, r_1))
+			sql = """INSERT INTO {table}(time, ip, phy_bler_0) VALUES(%s, %s, %s)""".format(table='phy_bler_parser')
+			# sql = """INSERT INTO {table}(time, ip, phy_bler_0, phy_bler_1) VALUES(%s, %s, %s, %s)""".format(table='phy_bler_parser')
+			ip = self._ip_parser()
+			# print(_time, ip)
+			cur.execute(sql, (_time, ip, r_0))
+			# cur.execute(sql, (_time, self._ip_parser(), r_0, r_1))
 			conn.commit()
 		except Exception as e:
 			# raise e
@@ -60,9 +78,11 @@ class bler_parser(object):
 if __name__ == '__main__':
 	bler = bler_parser()
 	while True:
-		r_0, r_1 = bler.parser()
+		r_0 = bler.parser()
+		# r_0, r_1 = bler.parser()
 		# mydb = infulx_db("172.32.3.196", 8086, 'admin', 'admin', 'influx')
 		# mydb.write(infulx_db.get_phy_bler(r_0, r_1))
 		_time = time.strftime("%Y-%m-%d-%H-%M-%S", time.gmtime())
-		bler.cloud_db(_time, r_0, r_1)
+		bler.cloud_db(_time, r_0)
+		# bler.cloud_db(_time, r_0, r_1)
 		sleep(3)
