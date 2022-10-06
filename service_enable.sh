9df4217b0845d62ea7d65146edeb8c789edbc2d8
ZL Chen <iec100535@gmail.com>
2022-10-06 11:49:20 +0800

    service_enable.sh

--- service_enable.sh
+++ service_enable.sh
@@ -18,4 +18,4 @@ systemctl status phy_parser_status.service | grep Active
 systemctl enable du_parser.service
 systemctl status du_parser.service | grep Loaded
 systemctl status du_parser.service | grep Active
-echo 'Service is enabled.'
+echo 'Service status is showed.'
